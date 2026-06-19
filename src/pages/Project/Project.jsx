2c552c66a860cdf82cc01b4a89cfd64752530c2e
Prosper Samuel <prospersamuel100@gmail.com>
2026-06-19 09:49:15 +0100

    Add missing newline for improved code formatting

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -24,6 +24,7 @@ import burgerHome2 from '../../assets/Projectimages/BurgerWebsite/Burgerhome2.pn
 import burgerOrderPage from '../../assets/Projectimages/BurgerWebsite/burgerOrderPage.png';
 import burgercheckout from '../../assets/Projectimages/BurgerWebsite/burgercheckout.png';
 
+
 import { ChevronLeft, ChevronRight, ChevronDown, ChevronUp, ExternalLink } from 'lucide-react';
 import { useApp } from "../../context/Appcontext";
 import { motion } from 'framer-motion';
